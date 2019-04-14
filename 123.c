1d8532e16f699ace53f0fe7a555f69cd716060a1
zhengyang0106 <49523368+zhengyang0106@users.noreply.github.com>
2019-04-14 09:47:15 +0800

    Update 123.c

--- 123.c
+++ 123.c
@@ -5,6 +5,5 @@
 	> Created Time: 2019年04月14日 星期日 09时44分53秒
  ************************************************************************/
 
-#include<stdio.h>gfjahfghjafgjasgfjaf
-ahgfasfljhasgfla
-
+#include<stdio.h>
+123456464546456
