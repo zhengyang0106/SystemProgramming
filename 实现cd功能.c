c51ff7c18277e682b88291ef4ec847dfe8aaf1a8
zhengyang <1021606521@qq.com>
2019-04-26 23:20:32 +0800

    ~替换家目录　屏蔽 ctrl + c

--- 实现cd功能.c
+++ 实现cd功能.c
@@ -14,30 +14,33 @@
 #include<signal.h>
 #define PATH_SIZE 100
 
+char buf1[80];
 char buf[80];//存放路径
 struct passwd *pwd;
 char hostname[60] = {'\0'};
+int home_length;
+
 void  cd_command() {
 
     char str1[10];
     char path[PATH_SIZE];
     int res;
-    memset(path,'\0',PATH_SIZE);                                         //初始化ｐａｔｈ数组
+    memset(path, '\0', PATH_SIZE);                                         //初始化ｐａｔｈ数组
 
     while(~ scanf("%s", str1)) {
         if (!strcmp(str1,"exit")) break;
-        scanf(" %s",path);
+        scanf(" %s", path);
         if (!strcmp(str1, "cd")) {
              res =  chdir(path);
             if(res != 0)
-               printf("%s is not a path,please check again\n",path);
+               printf("%s is not a path,please check again\n", path);
          }
-        else  printf("%s is not a command,please check\n",str1);
+        else  printf("%s is not a command,please check\n", str1);
 
-    getcwd(buf,sizeof(buf)); //把当前工作的绝对路径 复制到buf的数组里 
-   if(strlen(buf) < strlen("/home/zhengyang")) 
-    printf("\033[31m%s@%s:%s\n\033[0m",pwd->pw_name,hostname,buf);
-    else printf("\033[31m%s@%s:~%s\n\033[0m",pwd->pw_name,hostname,buf + strlen("/home/zhengyang"));
+    getcwd(buf, sizeof(buf)); //把当前工作的绝对路径 复制到buf的数组里 
+    if(strlen(buf) < home_length) 
+    printf("\033[34m%s@%s:%s\n\033[0m",pwd->pw_name,hostname,buf);
+    else printf("\033[33m%s@%s:~%s\n\033[0m", pwd->pw_name, hostname, buf + home_length);
          
    }
 }
@@ -46,27 +49,26 @@ void get_password() {
     
    struct passwd *qwe;
     qwe = getpwuid(getuid());
-    printf("%s\n",qwe->pw_passwd);                                       //获取密码
+    printf("%s\n", qwe->pw_passwd);                                       //获取密码
 }
 
 
 void get_username_hostname_path() {  
 
     gethostname(hostname, sizeof(hostname));                             //获取主机名
-    pwd = getpwuid(getuid());                                            //先用getuid()获取id号  通过 getwuid()函数通过uid 查看用户的passwd数据  获取用户名
-    getcwd(buf,sizeof(buf)); //把当前工作的绝对路径 复制到buf的数组里 
-   if(strlen(buf) < strlen("/home/zhengyang")) 
-    printf("\033[31m%s@%s:%s\n\033[0m",pwd->pw_name,hostname,buf);
-    else printf("\033[31m%s@%s:~%s\n\033[0m",pwd->pw_name,hostname,buf + strlen("/home/zhengyang"));
+    getcwd(buf, sizeof(buf)); //把当前工作的绝对路径 复制到buf的数组里 
+    if(strlen(buf) < home_length) 
+    printf("\033[31m%s@%s:%s\n\033[0m", pwd->pw_name, hostname, buf);
+    else printf("\033[31m%s@%s:~%s\n\033[0m", pwd->pw_name, hostname, buf + home_length);
 }
 
-
-
 int main(){
-    signal(SIGINT,SIG_IGN);
-get_username_hostname_path();
-cd_command();
-
+    pwd = getpwuid(getuid());                                            //先用getuid()获取id号  通过 getwuid()函数通过uid 查看用户的passwd数据  获取用户名
+  //home_length = strlen(getcwd(buf1, sizeof(buf1)));
+    home_length = strlen(pwd->pw_dir);                                   //在passwd结构体中有有一个pw_dir 成员变量－－>家目录    
+    signal(SIGINT, SIG_IGN);
+    get_username_hostname_path();
+    cd_command();
     return 0;
 }
 
